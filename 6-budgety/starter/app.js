fd131632740c7abc22a675c987c1db4af51f2114
Fahid Javid <fahidjavid@gmail.com>
2019-10-01 22:46:02 +0500

    Added input and dom structure.

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,89 @@
+/**
+ * Developer: Fahid Javid
+ */
+
+
+// Add event handler
+// Get input values
+// Add the new item to our data structure
+// Add the new item to the UI
+// Calculate budget
+// Update the UI
+
+
+// BUDGET CONTROLLER
+var budgetCOntroller = (function () {
+
+    // some code
+
+})();
+
+// UI CONTROLLER
+var UIController = (function () {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc/exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    }
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function () {
+
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 12 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+
+    };
+    var ctrlAddItem = function () {
+
+        // 1. Get the filed input data
+        var input = UICtrl.getInput();
+
+        console.log(input);
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+    };
+
+    return {
+        init: function () {
+            console.log('Application has been initiated!');
+
+            setupEventListeners();
+        }
+    }
+
+
+})(budgetCOntroller, UIController);
+
+controller.init();
