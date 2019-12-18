d05e18443bb1405833acb9b1e70a4d9b2b61fba7
Fahid Javid <fahidjavid@gmail.com>
2019-12-18 20:56:39 +0500

    Completed: 142 Building the Search View - Part 2

--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -10,6 +10,20 @@ export const clearResList = () => {
     elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title,limit = 17) => {
+    if(title.length > limit){
+        const newTitle = [];
+        title.split(' ').reduce((acc,cur)=>{
+            if(acc + cur.length <= limit){
+                newTitle.push(cur); 
+            }
+            return acc + cur.length;
+        }, 0);
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `
     <li id="${recipe.id}">
@@ -18,7 +32,7 @@ const renderRecipe = recipe => {
                 <img src="https://spoonacular.com/recipeImages/${recipe.image}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.readyInMinutes} Mins</p>
             </div>
         </a>
