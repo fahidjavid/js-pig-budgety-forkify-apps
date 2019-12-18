ecb6489bd9383570db48f5728f1f7766407ab11d
Fahid Javid <fahidjavid@gmail.com>
2019-12-18 18:30:56 +0500

    Add entery file of frokify project.

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,11 +1,39 @@
-import axios from 'axios';
-async function getResults(getResults){
-    const key = 'c25b91d186fe47a5abd96993b2789c07';
-    const res = await axios(`https://api.spoonacular.com/recipes/search?query=cheese&number=2&apiKey=${key}`);
-    console.log(res);
+import Search from './models/Search'
+
+/** Global state of the app
+* - Search object
+* - Current recipe object
+* - Shopping list object
+* - Liked recipes
+*/
+
+const state = {};
+
+async function controlSearch() {
+    // 1. Get search query
+    const query = 'pizza';
+
+    if(query){
+        // 2. New search object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for the results
+
+        // 4. Search for recipies
+        await state.search.getResults();
+
+        // Render results on UI
+        console.log(state.search.result);     
+    }
 }
 
-getResults();
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+const result = new Search('cheese&number=2');
+result.getResults();
 
 // API URL: https://api.spoonacular.com/recipes/search?query=cheese&number=2
 // apiKey: c25b91d186fe47a5abd96993b2789c07
