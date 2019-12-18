04de5faadae2d35e20ed562508f786f181106224
Fahid Javid <fahidjavid@gmail.com>
2019-12-18 18:31:17 +0500

    Added search model.

---
+++ 9-forkify/starter/src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults(){
+        const key = 'c25b91d186fe47a5abd96993b2789c07';
+
+        try{
+            const res = await axios(`https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${this.query}`);
+            this.result = res.data.results;
+            // console.log(this.result);
+        } catch(error){
+            alert(error);
+        }
+    }
+}
