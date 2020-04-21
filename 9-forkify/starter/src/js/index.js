b398de38f85f16f2eb689183a793ea9c189355bb
Fahid Javid <fahidjavid@gmail.com>
2020-04-21 15:22:44 +0500

    Completed: 148 Building the Recipe View - Part 1

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import {elements, renderLoader, clearLoader} from './views/base';
 
 /** Global state of the app
@@ -69,16 +70,19 @@ const controlRecipe = async () => {
 
 	if (id) {
 		// Prepare UI for changes
+		recipeView.clearRecipe();
+		renderLoader(elements.recipe);
 
 		// Create new recipe object
 		state.recipe = new Recipe(id);
-		console.log( 'Working on your request!' );
+
 		try {
 			// Get recipe data
 			await state.recipe.getRecipe();
 
 			// Render recipe
-			console.log(state.recipe);
+			clearLoader();
+			recipeView.renderRecipe(state.recipe);
 		} catch {
 			alert('Error processing recipe!');
 		}
